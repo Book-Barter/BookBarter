1f263e31d81ceeca9f2ebb224ac8e084e750284b
Mustafa Ozhan <mustafa.ozhan@massive.co>
2020-09-24 18:06:02 +0200

    Moving IOS to 14 and rearranging codes

---
+++ ios/ios/App/BookBarterApp.swift
@@ -0,0 +1,19 @@
+//
+//  BookBarterApp.swift
+//  ios
+//
+//  Created by Mustafa Ozhan on 24/09/2020.
+//  Copyright © 2020 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct BookBarterApp: App {
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
